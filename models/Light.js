8d61e9d51420e8c582ffae460a016b77f44de889
shortRound1911 <nsimone1911@gmail.com>
2015-04-07 17:59:09 -0400

    Revert "first commit for branch"
    This reverts commit 5f111762c98ed1609adb3bfacca84ed69cc00f08.

--- models/Light.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var lite = mongoose.Schema({
-  id : String,
-  type : String
-});
-
-module.exports = mongoose.model("Light", lite);
--- web/routes/roomsApi.js
+++
@@ -1,2 +0,0 @@
-var router = require("./baseApi");
-
